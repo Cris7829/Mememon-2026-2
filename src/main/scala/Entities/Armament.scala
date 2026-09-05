a00e2a2fc788c1efc012082d0bcc8412baed87dd
Cris7829 <cristobal.anasco.l@ug.uchile.cl>
2026-09-05 17:07:05 -0400

    Refactor(Armament): Translation of comments
    Translation of comments in Spanish to English.

--- src/main/scala/Entities/Armament.scala
+++ src/main/scala/Entities/Armament.scala
@@ -1,31 +1,30 @@
 package cl.uchile.dcc
 package Entities
 
-//Molde base de todos los armamentos
+//Base mold for all the Armaments
 abstract class Armament(n:String, a:Int, w:Int):
  protected val name: String = n
  protected var attack_points: Int = a
  protected var weight : Int = w
- 
-//Molde de armas magicas
+
+//Base mold for magic weapons such as Wand and Cane
 class Magic_Weapon(n:String, a:Int, w:Int, m_a:Int ) extends Armament(n, a, w):
   protected var magic_attack_points: Int = m_a
 
 
 
-//Armas magicas
+//Magic weapons with Magic-Weapons mold
 class Cane (n:String, a:Int, w:Int, m_a:Int ) extends Magic_Weapon(n, a, w, m_a)
 
 class Wand (n:String, a:Int, w:Int, m_a:Int ) extends Magic_Weapon(n, a, w, m_a)
 
 
-//Armas no magicas
+//Non-Magic Weapons, they don't need mold other than Armaments
 class Sword(n:String, a:Int, w:Int) extends Armament(n ,a ,w)
-  
+
 class Dagger(n:String, a:Int, w:Int) extends Armament(n ,a ,w)
-  
+
 class Bow(n:String, a:Int, w:Int) extends Armament(n ,a ,w)
-  
-  
-  
-  
+
+
+
