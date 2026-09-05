1a3e2633f35d651d2851906d20589f218952ef5c
Cris7829 <cristobal.anasco.l@ug.uchile.cl>
2026-09-05 17:20:32 -0400

    feat(Potion): implement base Potion class hierarchy
    Add abstract Potion base class and subclasses for Mana,
    Healing, MagicStrength, and PlayerStrength potions.

---
+++ src/main/scala/Entities/Potion.scala
@@ -0,0 +1,17 @@
+package cl.uchile.dcc
+package Entities
+
+\Base mold
+abstract class Potion (n:String):
+  protected val name: String=n
+
+
+
+\\Types of potions using Potion mold
+class Healing (n:String) extends Potion(n)
+
+class Mana (n:String) extends Potion(n)
+
+class Magic_Strength (n:String) extends Potion(n)
+
+class Strength_Player (n:String) extends Potion(n)
