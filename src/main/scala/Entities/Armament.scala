cddb2687ae0ca3ba6a6a79af1a770824f2d980b3
Cris7829 <cristobal.anasco.l@ug.uchile.cl>
2026-09-05 17:47:28 -0400

    Refactor(Armament): implement owner variable
    Add owner variable in the base mold of all the armaments.

--- src/main/scala/Entities/Armament.scala
+++ src/main/scala/Entities/Armament.scala
@@ -2,29 +2,30 @@ package cl.uchile.dcc
 package Entities
 
 //Base mold for all the Armaments
-abstract class Armament(n:String, a:Int, w:Int):
+abstract class Armament(n:String, a:Int, w:Int, o:String):
  protected val name: String = n
  protected var attack_points: Int = a
  protected var weight : Int = w
+ protected var owner : String = o
 
 //Base mold for magic weapons such as Wand and Cane
-class Magic_Weapon(n:String, a:Int, w:Int, m_a:Int ) extends Armament(n, a, w):
+class Magic_Weapon(n:String, a:Int, w:Int, o:String, m_a:Int ) extends Armament(n, a, w, o):
   protected var magic_attack_points: Int = m_a
 
 
 
 //Magic weapons with Magic-Weapons mold
-class Cane (n:String, a:Int, w:Int, m_a:Int ) extends Magic_Weapon(n, a, w, m_a)
+class Cane (n:String, a:Int, w:Int, o:String, m_a:Int ) extends Magic_Weapon(n, a, w, o ,m_a)
 
-class Wand (n:String, a:Int, w:Int, m_a:Int ) extends Magic_Weapon(n, a, w, m_a)
+class Wand (n:String, a:Int, w:Int, o:String, m_a:Int ) extends Magic_Weapon(n, a, w, o ,m_a)
 
 
 //Non-Magic Weapons, they don't need mold other than Armaments
-class Sword(n:String, a:Int, w:Int) extends Armament(n ,a ,w)
+class Sword(n:String, a:Int, w:Int, o:String) extends Armament(n ,a ,w ,o)
 
-class Dagger(n:String, a:Int, w:Int) extends Armament(n ,a ,w)
+class Dagger(n:String, a:Int, w:Int, o:String) extends Armament(n ,a ,w ,o)
 
-class Bow(n:String, a:Int, w:Int) extends Armament(n ,a ,w)
+class Bow(n:String, a:Int, w:Int, o:String) extends Armament(n ,a ,w ,o)
 
 
 
--- src/main/scala/Entities/Potion.scala
+++ src/main/scala/Entities/Potion.scala
@@ -1,13 +1,13 @@
 package cl.uchile.dcc
 package Entities
 
-\Base mold
+//Base mold
 abstract class Potion (n:String):
   protected val name: String=n
 
 
 
-\\Types of potions using Potion mold
+//Types of potions using Potion mold
 class Healing (n:String) extends Potion(n)
 
 class Mana (n:String) extends Potion(n)
